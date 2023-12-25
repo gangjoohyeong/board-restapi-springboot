769561a3b4256569846b54000a9c565b85d948ea
gangjoohyeng <bles@kakao.com>
2023-12-25 12:38:05 +0900

    feat: add hello world

--- src/main/java/com/restapi/board/BoardApplication.java
+++ src/main/java/com/restapi/board/BoardApplication.java
@@ -2,12 +2,28 @@ package com.restapi.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@RestController
+	public static class HelloController {
+
+		@GetMapping("/")
+		public Map<String, String> Hello() {
+			Map<String, String> map = new HashMap<>();
+			map.put("message", "Hello World");
+			return map;
+		}
 	}
 
 }
